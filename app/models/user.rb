6ab73e988871efaefcc5a4acbdb2ec3cb23f9bfe
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-06 12:31:12 -0300

    add relationships and #full_name to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :offers, dependent: :destroy, foreign_key: :owner
+  has_many :bookings, dependent: :destroy, foreign_key: :customer
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
