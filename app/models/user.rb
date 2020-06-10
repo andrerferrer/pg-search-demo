daae164090461bd6db06f74f85aeac41bb915f98
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-09 21:47:47 -0300

    add scope for users who are owners

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  # Associations
   has_many :offers, dependent: :destroy, foreign_key: :owner
   has_many :bookings, dependent: :destroy, foreign_key: :customer
 
+  # Scopes
+  scope :owner, -> { User.where(id: Offer.select(:owner_id).distinct) }
+
+  # Methods
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
