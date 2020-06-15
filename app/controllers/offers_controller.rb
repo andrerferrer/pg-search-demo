f38a3623cdd228c9f305b35c8ead966e297cc099
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-15 20:47:48 -0300

    search in the controller with the description

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,14 @@
 class OffersController < ApplicationController
   def index
     @offers = Offer.all
+    search if params[:search]
+  end
+
+  private
+
+  def search
+    search = params[:search]
+    @offers = @offers.where(description: search[:description]) if search[:description].present?
+    end
   end
 end
