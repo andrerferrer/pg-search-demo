201eef1178a7ba4dc6d796cf42b710dfb75c3d06
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-15 21:21:09 -0300

    add PG search to the model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,20 @@
 class Offer < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :bookings, dependent: :destroy
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  pg_search_scope :search_by_owner,
+    associated_against: {
+      owner: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
