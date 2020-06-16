f37f04464f21880ef8f86d3d9570400b0d36fc27
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-15 21:22:34 -0300

    Rubocop Offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 class Offer < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :bookings, dependent: :destroy
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_description,
-    against: [ :name, :description ],
-    using: {
-      tsearch: { prefix: true }
-    }
+                  against: %i[name description],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 
   pg_search_scope :search_by_owner,
-    associated_against: {
-      owner: [ :first_name, :last_name ]
-    },
-    using: {
-      tsearch: { prefix: true }
-    }
-
+                  associated_against: {
+                    owner: %i[first_name last_name]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
