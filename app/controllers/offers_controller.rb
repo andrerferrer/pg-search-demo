b79810d9620a63afdc4e97878cc96684aa07f465
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-15 21:10:19 -0300

    Add searching by owner

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,9 @@
 class OffersController < ApplicationController
   def index
     @offers = Offer.all
+    owners = User.owners
+    @owners_names = owners.map(&:full_name)
+    @owners_ids = owners.pluck :id
     search if params[:search]
   end
 
@@ -9,5 +12,13 @@ class OffersController < ApplicationController
   def search
     search = params[:search]
     @offers = @offers.where('name @@ :name', name: "%#{search[:name]}%") if search[:name].present?
+    if search[:owner].present?
+      user = User.find search[:owner]
+      @offers = @offers.joins(:owner)
+                       .where(users: {
+                         first_name: user.first_name,
+                         last_name: user.last_name
+                       })
+    end
   end
 end
